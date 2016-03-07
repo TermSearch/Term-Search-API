5e0c3adbbdaf6aded67506959a372c9c1ed62b76
Koen van Gilst <koen.vangilst@gmail.com>
2016-03-07 14:35:33 +0100

    adds attributes for tmx de + nl

---
+++ api/models/Translation.js
@@ -0,0 +1,15 @@
+/**
+ * Translation.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    'docNr': { type: 'string' },
+    'DE-DE': { type: 'string' },
+    'NL-NL': { type: 'string' }
+  }
+};
