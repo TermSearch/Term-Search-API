42e2b9c69d6519da82ce0449d64450547c5e0790
Koen van Gilst <koen.vangilst@gmail.com>
2016-03-12 16:16:42 +0100

    adds native implementation of mongodb text search

--- common/models/translation.js
+++ common/models/translation.js
@@ -1,29 +1,46 @@
-var app = require('..')
+module.exports = function (Translation) {
 
-module.exports = function(Translation) {
+	Translation.status = function (cb) {
+		var TranslationCollection = Translation.dataSource.connector.collection('translation');
+		TranslationCollection.find({
+				$text: {
+					$search: 'Anlage'
+				}
+			})
+      .sort({ "_id": 1}).skip(0).limit(100)
+			.toArray(
+				function (err, count) {
+					console.log(count);
+          var response = count;
+          cb(null, response);
+				});
 
-  Translation.status = function (cb) {
-  	var currentDate = new Date();
-  	var currentHour = currentDate.getHours();
-  	var OPEN_HOUR = 6;
-  	var CLOSE_HOUR = 20;
-  	console.log('Current hour is ' + currentHour);
-  	var response;
-  	if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
-  		response = 'We are open for business.';
-  	} else {
-  		response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
-  	}
+		// var currentDate = new Date();
+		// var currentHour = currentDate.getHours();
+		// var OPEN_HOUR = 6;
+		// var CLOSE_HOUR = 20;
+		// console.log('Current hour is ' + currentHour);
+		// var response;
+		// if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+		// 	response = 'We are open for business.';
+		// } else {
+		// 	response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
+		// }
 
-  	cb(null, response);
-  };
 
-  Translation.remoteMethod(
-    'status',
-    {
-      http: {path: '/status', verb: 'get'},
-      returns: {arg: 'status', type: 'string'}
-    }
-  );
+	};
+
+	Translation.remoteMethod(
+		'status', {
+			http: {
+				path: '/textsearch',
+				verb: 'get'
+			},
+			returns: {
+				arg: 'status',
+				type: 'string'
+			}
+		}
+	);
 
 };
