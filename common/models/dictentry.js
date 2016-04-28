02cf9cecbc41452f5b2bfa848ee80de862ecb1f5
Koen van Gilst <koen.vangilst@gmail.com>
2016-04-28 10:23:07 +0200

    speeding up live search < 5 ms on localhost

--- common/models/dictentry.js
+++ common/models/dictentry.js
@@ -5,11 +5,60 @@ const dictEntryHelper = require('../helpers/dictEntryHelper');
 
 module.exports = function (Dictentry) {
 
-	Dictentry.liveSearch = function (term, subjectFields = false, limit, skip, cb) {
+	// live search query optimised for speed, case sensitive
+	Dictentry.liveSearch = function (term, subjectFields = false, limit = 10, skip = 0, cb) {
 
 		const whereQuery = {
-			de: {like: '^'+term } // live search query optimised for speed, case sensitive
-		}
+			de: {like: '^'+term }
+		};
+
+		console.time('Q: '+term+' - Subject: '+subjectFields);
+
+		if (subjectFields) {
+      // Convert the comma seperated string to an array of subjectField numbers
+      const subjectFieldNrArr = SubjectField.toNrArr(subjectFields);
+			// Map all subjectFields to AND query
+			const andQuery = subjectFieldNrArr.map( nr => ({ subjectFields: nr }) );
+			// Add to whereQuery
+      whereQuery.and = andQuery;
+    }
+
+		// Build query
+		const query = {
+			where: whereQuery,
+			order: 'de ASC',  	// sort de ascending
+			limit: limit,
+      skip: skip,
+			fields: {
+					'de': true // only return 'de' string
+				}
+		};
+
+		Dictentry.find(query)
+		  .then(dictentries => {
+		    cb(null, {
+		      dictentries
+		    });
+		  })
+		  .then(() => {
+		    // log query time
+		    console.timeEnd('Q: ' + term + ' - Subject: ' + subjectFields);
+		  });
+	};
+
+	Dictentry.startsWith = function (term, subjectFields = false, limit = 10, skip = 0, cb) {
+
+    const regexQuery = new RegExp('^' + term, 'i');
+
+		// Regex queries are not very fast, maybe someday use this for more speed?
+		// or: [
+		//   {de: {like: '^Test'}},
+		//   {de: {like: '^test'}}
+		//   ]
+
+		const whereQuery = {
+			de: regexQuery
+		};
 
 		console.time('Q: '+term+' - Subject: '+subjectFields);
 
@@ -43,59 +92,9 @@ module.exports = function (Dictentry) {
 						});
 					})
 					.then( () => {
-						console.timeEnd('Q: '+term+' - Subject: '+subjectFields); // log query time
-					})
-			});
-	};
-
-	Dictentry.startsWith = function (term, subjectFields = false, limit, skip, cb) {
-
-    const regexQuery = new RegExp('^' + term, 'i');
-
-		// Regex queries are not very fast, maybe someday use this for more speed?
-		// or: [
-		//   {de: {like: '^Test'}},
-		//   {de: {like: '^test'}}
-		//   ]
-
-		const whereQuery = {
-			de: regexQuery
-		}
-
-		console.time(term);
-
-		if (subjectFields) {
-      // Convert the comma seperated string to an array of subjectField numbers
-      const subjectFieldNrArr = SubjectField.toNrArr(subjectFields);
-			// Map all subjectFields to AND query
-			const andQuery = subjectFieldNrArr.map( nr => ({ subjectFields: nr }) );
-			// Add to whereQuery
-      whereQuery.and = andQuery;
-    }
-
-		// Build query
-		const query = {
-			where: whereQuery,
-			order: 'de ASC',  	// sort de ascending
-			limit: limit,
-      skip: skip
-		};
-
-		// First do a count with only the whereQuery
-		Dictentry.count(whereQuery)
-			.then(count => {
-				// Then execute the actual search query
-				Dictentry.find(query)
-					.then(dictEntryHelper.resolveSubjectFields)
-					.then(dictentries => {
-						cb(null, {
-							count,
-							dictentries
-						});
-					})
-					.then( () => {
-						console.timeEnd(term); // log query time
-					})
+						// log query time
+						console.timeEnd('Q: '+term+' - Subject: '+subjectFields);
+					});
 			});
 	};
 
